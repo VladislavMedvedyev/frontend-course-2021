d5d2acb38c06c299588c9a4ee87b653ac393ab88
VladislavMedvedyev <vlad.medvedyev@gmail.com>
2021-12-20 10:52:11 +0200

    Add player change logic on typescript Add angular eslint

--- homeworks/vladyslav.medvediev_VladislavMedvedyev/10-components/components/src/app/app.component.ts
+++ homeworks/vladyslav.medvediev_VladislavMedvedyev/10-components/components/src/app/app.component.ts
@@ -7,4 +7,70 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'components';
+
+  changePlayer(): void {
+      if (playerTurn) {
+          playerTurn = false;
+          playerTurnShow();
+          item1Change();
+          item1Color();
+      } else {
+          playerTurn = true; 
+          playerTurnShow();
+          item1Change();
+          item1Color();
+      }
+  }
+
+  resetCurrentGame(): void {
+      newArr = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+      item1Color();
+      console.log('resetCurrentGame');
+  }
+}
+
+let playerTurn: boolean;
+
+function playerTurnShow(): void {
+    const player1TurnEl = document.querySelector('[data-player-1-turn]');
+    const player2TurnEl = document.querySelector('[data-player-2-turn]');
+    if (playerTurn) {
+        console.log('player1');
+        player1TurnEl?.classList.add('player-1-turn-container');
+        player2TurnEl?.classList.remove('player-2-turn-container-visible');
+    } else {
+        console.log('player2');
+        player1TurnEl?.classList.remove('player-1-turn-container');
+        player2TurnEl?.classList.add('player-2-turn-container-visible');
+    }
+}
+
+window.onload = function () {
+    playerTurn = true;
+    playerTurnShow();
+}
+
+let newArr = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+function item1Change() {
+    if(playerTurn) {
+        newArr[0] = 1;
+    } else {
+        newArr[0] = 2;
+    }
+}
+
+function item1Color() {
+    const item1Player1 = document.querySelector('[data-item-1-player-1]');
+    const item1Player2 = document.querySelector('[data-item-1-player-2]');
+    if (newArr[0] == 0) {
+        item1Player1?.classList.remove('item-1-player-1-visible');
+        item1Player2?.classList.remove('item-1-player-2-visible');
+    } else if (newArr[0] == 1) {
+        item1Player1?.classList.remove('item-1-player-1-visible');
+        item1Player2?.classList.add('item-1-player-2-visible');
+    } else {
+        item1Player1?.classList.add('item-1-player-1-visible');
+        item1Player2?.classList.remove('item-1-player-2-visible');
+    }
 }
